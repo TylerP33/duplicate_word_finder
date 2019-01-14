f9228c2c564d34b48b82502ad0a3794bd06514d5
Tyler Pulse <32463199+TylerP33@users.noreply.github.com>
2019-01-14 11:32:27 -0600

    Delete span on backspace

--- index.js
+++ index.js
@@ -4,36 +4,34 @@ $(document).ready(function() {
 	let input = []
 	let wordBank = []
 
-const placeCaretAtEnd = (el) => {
-    el.focus();
-    if (typeof window.getSelection != "undefined"
-            && typeof document.createRange != "undefined") {
-        var range = document.createRange();
-        range.selectNodeContents(el);
-        range.collapse(false);
-        var sel = window.getSelection();
-        sel.removeAllRanges();
-        sel.addRange(range);
-    } else if (typeof document.body.createTextRange != "undefined") {
-        var textRange = document.body.createTextRange();
-        textRange.moveToElementText(el);
-        textRange.collapse(false);
-        textRange.select();
-    }
-}
+	const placeCaretAtEnd = (el) => {
+    	el.focus();
+    	if (typeof window.getSelection != "undefined" && typeof document.createRange != "undefined") {
+        		var range = document.createRange();
+        			range.selectNodeContents(el);
+        			range.collapse(false);
+        			var sel = window.getSelection();
+        			sel.removeAllRanges();
+        			sel.addRange(range);
+    	} else if (typeof document.body.createTextRange != "undefined") {
+        			var textRange = document.body.createTextRange();
+        			textRange.moveToElementText(el);
+        			textRange.collapse(false);
+        			textRange.select();
+	    }
+	 }
 
-// callback is a variable that will do our magic.  It will be the function that will handle our async progress
-	const words = (callback) => {
+	const gatherInput = (callback) => {
 		editor.on('input', function(){
-			if (!editor.html().includes('&nbsp')){
+			if (editor.html()){
 				input.push(editor.html().replace(/[\/#!$%\^&.\,?*;:{}=\-_`~()]/g,"").toLowerCase())
-				console.log(input)
 			}
 
 			let last = input[input.length - 1].split(" ").sort()
 			for(let i = 0; i <= last.length; i++){
-				if (last[i] === last[i + 1] && last[i] !== undefined && !wordBank.includes(last[i])){
+				if (last[i] === last[i + 1] && last[i] !== undefined && !wordBank.includes(last[i]) && !last[i].includes(`style"`)){
 					wordBank.push(last[i])
+					console.log(wordBank)
 					callback()
 				}
 			}	
@@ -41,25 +39,46 @@ const placeCaretAtEnd = (el) => {
 
 	}
 
-
 	const appendDuplicate = () => {
 		let appendedWord = wordBank.filter( function( item, index, inputArray ) {return inputArray.indexOf(item) == index; })
 		let color =  "#" + Math.random().toString(16).slice(2, 8);
 			if 	(!$("#wordBank").html().includes(appendedWord[appendedWord.length - 1])) {
-					$("#wordBank").append('<span style=background-color:' + color + '>' + "\n" + appendedWord[appendedWord.length - 1] + "</span>")
+					$("#wordBank").append(' <span style=background-color:' + color + '>' + "\n" + appendedWord[appendedWord.length - 1] + "</span>")
 					var match = new RegExp(appendedWord[appendedWord.length - 1], 'g')
-					let mug = editor.html().replace(match, '<span style=background-color:' + color + '>' + "\n" + appendedWord[appendedWord.length - 1] + "</span>")
-					editor.html(mug)
+					let addColor = editor.html().replace(match, '<span style=background-color:' + color + '>' + "\n" + appendedWord[appendedWord.length - 1] + "</span>")
+					editor.html(addColor)
 					placeCaretAtEnd(editor.get(0));
 			}
 			
 	}
 
-	const highlightWord = () => {
+	editor.on('keydown', function(event) {
+    // Check for a backspace
+    if (event.which == 8) {
+        s = window.getSelection();
+        r = s.getRangeAt(0)
+        el = r.startContainer.parentElement
+        // Check if the current element is the .label
+        if (el.classList.contains('label')) {
+            // Check if we are exactly at the end of the .label element
+            if (r.startOffset == r.endOffset && r.endOffset == el.textContent.length) {
+                // prevent the default delete behavior
+                event.preventDefault();
+                if (el.classList.contains('highlight')) {
+                    // remove the element
+                    el.remove();
+                        editor.html("")
+                } else {
+                    el.classList.add('highlight');
+                }
+                return;
+            }
+        }
+    }
+    event.target.querySelectorAll('span.label.highlight').forEach(function(el) { el.classList.remove('highlight');})
+});
 
-	}
 
-
-	words(appendDuplicate)
+	gatherInput(appendDuplicate)
 });
 

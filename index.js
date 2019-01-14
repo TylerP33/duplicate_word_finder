7aecf308e05b6dcb6c9f722d8cc3f1730135a8d7
Tyler Pulse <32463199+TylerP33@users.noreply.github.com>
2019-01-14 09:23:50 -0600

    Refactored
    Broke previous code down into functions.
    
    Readability is much greater

--- index.js
+++ index.js
@@ -1,37 +1,65 @@
 $(document).ready(function() {
 	let editor = $('#textEditor')
+	editor.attr('contenteditable','true');
 	let input = []
 	let wordBank = []
 
-	editor.attr('contenteditable','true');
+const placeCaretAtEnd = (el) => {
+    el.focus();
+    if (typeof window.getSelection != "undefined"
+            && typeof document.createRange != "undefined") {
+        var range = document.createRange();
+        range.selectNodeContents(el);
+        range.collapse(false);
+        var sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+    } else if (typeof document.body.createTextRange != "undefined") {
+        var textRange = document.body.createTextRange();
+        textRange.moveToElementText(el);
+        textRange.collapse(false);
+        textRange.select();
+    }
+}
 
-	editor.on('input', function(){
-		if (editor.html() !== '&nbsp'){
-			input.push(editor.html().toLowerCase())
-		}
-		let last = input[input.length - 1].split(" ")
-		for(let i = 0; i < last.sort().length; i++){
-			if (last.sort()[i] === last.sort()[i + 1]){
-				wordBank.push(last[i])
-				appendedWord = wordBank.filter( function( item, index, inputArray ) {
-           			return inputArray.indexOf(item) == index;
-    			});
-    			if (!$("#wordBank").html().includes(appendedWord[appendedWord.length - 1])) {
-    					let color =  "#" + Math.random().toString(16).slice(2, 8);
-						$("#wordBank").append('<span style=background-color:' + color + '>' + "\n" + appendedWord[appendedWord.length - 1] + "</span>")
-						//$("#lol").html().replace(appendedWord[appendedWord.length - 1], 'good')
-						// when we find a duplicate, highlight both
-						// need to go into innerHTML, find the selected word, and highlight it with color
-					}
-				}
+// callback is a variable that will do our magic.  It will be the function that will handle our async progress
+	const words = (callback) => {
+		editor.on('input', function(){
+			if (!editor.html().includes('&nbsp')){
+				input.push(editor.html().replace(/[\/#!$%\^&.\,?*;:{}=\-_`~()]/g,"").toLowerCase())
+				console.log(input)
 			}
+
+			let last = input[input.length - 1].split(" ").sort()
+			for(let i = 0; i <= last.length; i++){
+				if (last[i] === last[i + 1] && last[i] !== undefined && !wordBank.includes(last[i])){
+					wordBank.push(last[i])
+					callback()
+				}
+			}	
 		})
-	});
+
+	}
 
 
-	//if (editor.html().includes(appendedWord[appendedWord.length - 1])){
-	//						holder.push(editor.html())
-	//						let last = holder.join("").split(" ")
-	//						// editor.val(last[last.length - 1]).css("color", color)
-	//						let f = editor.find(appendedWord[appendedWord.length - 1]).css("color", 'red')
-	//					}
+	const appendDuplicate = () => {
+		let appendedWord = wordBank.filter( function( item, index, inputArray ) {return inputArray.indexOf(item) == index; })
+		let color =  "#" + Math.random().toString(16).slice(2, 8);
+			if 	(!$("#wordBank").html().includes(appendedWord[appendedWord.length - 1])) {
+					$("#wordBank").append('<span style=background-color:' + color + '>' + "\n" + appendedWord[appendedWord.length - 1] + "</span>")
+					var match = new RegExp(appendedWord[appendedWord.length - 1], 'g')
+					let mug = editor.html().replace(match, '<span style=background-color:' + color + '>' + "\n" + appendedWord[appendedWord.length - 1] + "</span>")
+					editor.html(mug)
+					placeCaretAtEnd(editor.get(0));
+			}
+			
+	}
+
+	const highlightWord = () => {
+
+	}
+
+
+	words(appendDuplicate)
+});
+
